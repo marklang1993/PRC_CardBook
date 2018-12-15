861e854633eb8bce2f9b3073d1be36f656830306
Mark Lang <marklang1993@gmail.com>
2018-12-16 02:04:33 +0900

    Able to display preview and decode QRCode.

--- PRC_CardBook/app/src/main/java/com/swws/marklang/prc_cardbook/activity/qrcode/ScannerActivity.java
+++ PRC_CardBook/app/src/main/java/com/swws/marklang/prc_cardbook/activity/qrcode/ScannerActivity.java
@@ -3,6 +3,7 @@ package com.swws.marklang.prc_cardbook.activity.qrcode;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -10,7 +11,9 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
 import android.media.ImageReader;
 import android.os.Handler;
 import android.support.annotation.NonNull;
@@ -23,9 +26,20 @@ import android.view.Surface;
 import android.view.TextureView;
 import android.widget.Toast;
 
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.FormatException;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.PlanarYUVLuminanceSource;
+import com.google.zxing.Reader;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
 import com.swws.marklang.prc_cardbook.R;
 
-import java.util.Arrays;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class ScannerActivity extends AppCompatActivity {
 
@@ -33,11 +47,15 @@ public class ScannerActivity extends AppCompatActivity {
 
     private String mCameraId;
     private CameraDevice mCameraDevice;
-    private CameraCaptureSession mCameraCaptureSession;
-    private CaptureRequest.Builder mCaptureRequestBuilder;
+    private CaptureRequest.Builder mCaptureRequestBuilderSurfaceTexture;
+    private CaptureRequest.Builder mCaptureRequestBuilderQRCodeReader;
     private ImageReader mImageReader;
     private Size mImageSize;
     private Handler mBackgroundHandler;
+
+    private ArrayList<Surface> mSurfaces;
+
+    private Semaphore mCapturePicture = new Semaphore(1);
 
     private static final int CAMERA_INDEX = 0;
     private static final int REQUEST_CAMERA_PERMISSION = 200;
@@ -99,48 +117,66 @@ public class ScannerActivity extends AppCompatActivity {
 
     /**
      * Create Camera Preview
+     * @return Surface that links to CaptureRequestBuilder
      */
-    private void createCameraPreview() {
+    private Surface createCameraPreview() {
         try {
             // Init. SurfaceTexture
             SurfaceTexture surfaceTexture = mQRCodeTextureView.getSurfaceTexture();
             surfaceTexture.setDefaultBufferSize(mImageSize.getWidth(), mImageSize.getHeight());
 
-            // Init. Surface and mCaptureRequestBuilder
+            // Init. mCaptureRequestBuilderSurfaceTexture
             Surface surface = new Surface(surfaceTexture);
-            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            mCaptureRequestBuilder.addTarget(surface);
+            mCaptureRequestBuilderSurfaceTexture = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            mCaptureRequestBuilderSurfaceTexture.addTarget(surface); // Add the surfaceTexture (preview) to the capture target
 
-            // Create image capture session
-            mCameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
-                @Override
-                public void onConfigured(@NonNull CameraCaptureSession session) {
-                    if (mCameraDevice == null) {
-                        Log.e(getClass().getName(), "mCameraDevice is NULL!");
-                        return;
-                    }
-                    mCameraCaptureSession = session;
-                    updatePreview();
-                }
-
-                @Override
-                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-                    Log.e(getClass().getName(), "Configure failed when create capture session!");
-                    Toast.makeText(getApplicationContext(), R.string.exception_configure_failed_camera_capture_session, Toast.LENGTH_LONG).show();
-                }
-            }, null);
+            return surface;
 
         } catch (CameraAccessException ex) {
             // Failed to access camera
             Toast.makeText(getApplicationContext(), R.string.exception_access_camera_failed, Toast.LENGTH_LONG).show();
+            return null;
         }
     }
 
-    CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+    /**
+     * Create QRCode Image
+     * @return Surface that links to CaptureRequestBuilder
+     */
+    private Surface createQRCodeImage() {
+        try {
+            // Init. mCaptureRequestBuilderQRCodeReader
+            Surface surface = mImageReader.getSurface();
+            mCaptureRequestBuilderQRCodeReader = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            mCaptureRequestBuilderQRCodeReader.addTarget(surface); // Add the mImageReader (zxing) to the capture target
+
+            return surface;
+
+        } catch (CameraAccessException ex) {
+            // Failed to access camera
+            Toast.makeText(getApplicationContext(), R.string.exception_access_camera_failed, Toast.LENGTH_LONG).show();
+            return null;
+        }
+    }
+
+    /**
+     * Callback function fur init. surfaces and
+     */
+    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
         @Override
         public void onOpened(@NonNull CameraDevice camera) {
             mCameraDevice = camera;
-            createCameraPreview();
+
+            // Linked & Get Surfaces
+            Surface cameraPreviewSurface = createCameraPreview();
+            Surface QRCodeImageSurface = createQRCodeImage();
+
+            mSurfaces = new ArrayList<>(2);
+            mSurfaces.add(cameraPreviewSurface);
+            mSurfaces.add(QRCodeImageSurface);
+
+            // Start an image capture session
+            startImageCaptureSession(mSurfaces);
         }
 
         @Override
@@ -156,16 +192,92 @@ public class ScannerActivity extends AppCompatActivity {
     };
 
     /**
+     * Bind Surfaces
+     * @param surfaces
+     */
+    private void startImageCaptureSession(ArrayList<Surface> surfaces)
+    {
+        try {
+            // Create image capture session
+            mCameraDevice.createCaptureSession(surfaces, new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    if (mCameraDevice == null) {
+                        Log.e(getClass().getName(), "mCameraDevice is NULL!");
+                        return;
+                    }
+
+                    // Start from updating preview
+                    updatePreview(session);
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                    Log.e(getClass().getName(), "Configure failed when create capture session!");
+                    Toast.makeText(getApplicationContext(), R.string.exception_configure_failed_camera_capture_session, Toast.LENGTH_LONG).show();
+                }
+            }, null);
+
+        } catch (CameraAccessException ex) {
+            // Failed to access camera
+            Toast.makeText(getApplicationContext(), R.string.exception_access_camera_failed, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
      * Update Preview
      */
-    private void updatePreview() {
+    private void updatePreview(CameraCaptureSession cameraCaptureSession) {
         if (mCameraDevice == null)  {
             Log.e(getClass().getName(), "mCameraDevice is NULL!");
             return;
         }
-        mCaptureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO);
+        mCaptureRequestBuilderSurfaceTexture.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO);
         try {
-            mCameraCaptureSession.setRepeatingRequest(mCaptureRequestBuilder.build(), null, mBackgroundHandler);
+            // Capture a low-quality image for preview
+            cameraCaptureSession.setRepeatingRequest(mCaptureRequestBuilderSurfaceTexture.build(),
+                    new CameraCaptureSession.CaptureCallback() {
+                        @Override
+                        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+                            // Once finished, try to update QRCode reader
+                            try {
+                                if (mCapturePicture.tryAcquire()) {
+                                    session.stopRepeating();
+                                    updateQRCodeReader(session);
+                                }
+                            } catch (CameraAccessException e) {
+                                // Failed to access camera
+                                Toast.makeText(getApplicationContext(), R.string.exception_access_camera_failed, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }, mBackgroundHandler);
+
+        } catch (CameraAccessException ex) {
+            // Failed to access camera
+            Toast.makeText(getApplicationContext(), R.string.exception_access_camera_failed, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * Update QRCode Reader
+     * @param cameraCaptureSession
+     */
+    private void updateQRCodeReader(CameraCaptureSession cameraCaptureSession) {
+        if (mCameraDevice == null)  {
+            Log.e(getClass().getName(), "mCameraDevice is NULL!");
+            return;
+        }
+        mCaptureRequestBuilderQRCodeReader.set(CaptureRequest.CONTROL_MODE, CaptureRequest.CONTROL_MODE_AUTO);
+        try {
+            // Capture a high-quality image once for QRCode decoder
+            cameraCaptureSession.capture(mCaptureRequestBuilderQRCodeReader.build(),
+                    new CameraCaptureSession.CaptureCallback() {
+                        @Override
+                        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+                            // Once finished, restart updating preview
+                            updatePreview(session);
+                        }
+                    }, mBackgroundHandler);
 
         } catch (CameraAccessException ex) {
             // Failed to access camera
@@ -184,6 +296,44 @@ public class ScannerActivity extends AppCompatActivity {
             CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(mCameraId);
             StreamConfigurationMap streamConfigurationMap = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
             mImageSize = streamConfigurationMap.getOutputSizes(SurfaceTexture.class)[0];
+
+            // Init. ImageReader (JPEG format, maximum 1 image)
+            mImageReader = ImageReader.newInstance(mImageSize.getWidth(), mImageSize.getHeight(), ImageFormat.YUV_420_888, 1);
+            mImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+                @Override
+                public void onImageAvailable(ImageReader imageReader) {
+                    Image previewImage = imageReader.acquireLatestImage();
+                    ByteBuffer previewByteBuffer = previewImage.getPlanes()[0].getBuffer();
+                    byte[] previewBytes = new byte[previewByteBuffer.capacity()];
+                    previewByteBuffer.get(previewBytes);
+
+                    int imageWidth = previewImage.getWidth();
+                    int imageHeight = previewImage.getHeight();
+                    previewImage.close();
+
+                    PlanarYUVLuminanceSource luminanceSource = new PlanarYUVLuminanceSource(
+                            previewBytes, imageWidth, imageHeight,
+                            0, 0, imageWidth, imageHeight,
+                            false);
+                    BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(luminanceSource));
+                    Reader qrReader = new MultiFormatReader();
+                    try {
+                        Result result = qrReader.decode(binaryBitmap);
+                        Log.i(getClass().getName(), "Decoded: " + result.getText());
+
+                    } catch (FormatException e) {
+                        Log.e(getClass().getName(), "FormatException");
+
+                    } catch (ChecksumException e) {
+                        Log.e(getClass().getName(), "ChecksumException");
+
+                    } catch (NotFoundException e) {
+                        Log.e(getClass().getName(), "NotFoundException");
+                    }
+
+                    mCapturePicture.release();
+                }
+            }, mBackgroundHandler);
 
             // Check runtime camera permission since API 23
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
