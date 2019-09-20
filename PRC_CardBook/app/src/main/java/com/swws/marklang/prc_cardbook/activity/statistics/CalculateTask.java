57534edaa19bfafe9eb78b2d91414ec91419c28b
Mark Lang <marklang1993@gmail.com>
2019-09-20 11:26:48 +0900

    Bug fix: Instead of counting one JR item as 1 item in Statistics, count one JR item as 8 items since it has 8 variations.

--- PRC_CardBook/app/src/main/java/com/swws/marklang/prc_cardbook/activity/statistics/CalculateTask.java
+++ PRC_CardBook/app/src/main/java/com/swws/marklang/prc_cardbook/activity/statistics/CalculateTask.java
@@ -2,6 +2,7 @@ package com.swws.marklang.prc_cardbook.activity.statistics;
 
 import android.os.AsyncTask;
 
+import com.swws.marklang.prc_cardbook.activity.card.CardDetailActivity;
 import com.swws.marklang.prc_cardbook.activity.main.MainActivity;
 import com.swws.marklang.prc_cardbook.utility.database.Database;
 import com.swws.marklang.prc_cardbook.utility.database.Item;
@@ -25,14 +26,32 @@ public class CalculateTask extends AsyncTask<Void, Void, Void> {
     @Override
     protected Void doInBackground(Void... voids) {
         Database database = MainActivity.getDatabaseByIndex(mPosition);
-        int size = database.size();
+        int size = 0;
         int countOfPossessedItem = 0;
 
         // Do calculation
         for (Item item: database) {
             int inventoryOfCurrentItem = InventoryUtility.getInventoryCount(database.seasonId(), item);
-            if (inventoryOfCurrentItem > 0) {
-                ++countOfPossessedItem;
+
+            // Check is this a JR item
+            if (item.Rarity.equals("JR")) {
+                // 1. JR item
+                size += 8; // All JR item contains variations with 8 different colors
+                // Iterate the inventory count of all variations
+                for (int i = 0; i < 8; ++i) {
+                    int shiftBits = 3 * i;
+                    int currentCount = (inventoryOfCurrentItem >>> shiftBits) & CardDetailActivity.MAX_JR_INVENTORY_COUNT;
+                    if (currentCount > 0) {
+                        ++countOfPossessedItem;
+                    }
+                }
+
+            } else {
+                // 2. Non-JR item
+                ++size;
+                if (inventoryOfCurrentItem > 0) {
+                    ++countOfPossessedItem;
+                }
             }
         }
 
